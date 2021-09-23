7519e4919f2a38d8538a2484ec7812496e1ebf57
sjkchang <sjkchang@gmail.com>
2021-09-23 09:51:38 -0700

    Connected mysql database, and setup basic views engine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 const express = require("express");
+var mysql = require("mysql");
+
 const app = express();
 const port = 3000;
 
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "mysql_user",
+    password: "password",
+    database: "157a_team9",
+});
+
+connection.connect();
+
+connection.query("USE 157a_team9", function (err, rows, fields) {
+    if (err) throw err;
+});
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    connection.query(
+        "SELECT * FROM 157a_team9.students;",
+        function (err, rows, fields) {
+            if (err) throw err;
+            console.log(rows);
+            res.render("index", {
+                data: rows,
+            });
+        }
+    );
 });
 
 app.listen(port, () => {
