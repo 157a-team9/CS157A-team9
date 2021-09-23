b8c654f3a301cc9e5db4a8d89005afc1c68707fb
sjkchang <sjkchang@gmail.com>
2021-09-23 13:18:55 -0700

    Updated README and added comments

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
+# meckeys
+
 ![alt text](https://github.com/157a-team9/CS157A-team9/blob/main/working_demo.PNG)
+
+## Installation
+
+Download [Node.js](https://nodejs.org/).
+
+Clone the repository
+
+```sh
+cd Desktop   *(Replace desktop to the path of wherever you want the files to be)*
+git clone https://github.com/157a-team9/CS157A-team9.git
+```
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+cd CS157A-team9
+npm install
+npm start
+```
+
+You will also need to setup a new schema and user in mysql. The schema/database name should be 157a_team9. The user should be mysql_user. The users password should be "password".
+
+In the database make a table called students, with columns SJSU_ID, name, major, and add an entry to the table.
--- src/index.js
+++ src/index.js
@@ -1,38 +1,42 @@
+//imports express
 const express = require("express");
+
+//imports a mysql connetor module
 var mysql = require("mysql");
 
+//Creates an express webapp
 const app = express();
 const port = 3000;
 
+//connects to mysql server
 var connection = mysql.createConnection({
     host: "localhost",
     user: "mysql_user",
     password: "password",
     database: "157a_team9",
 });
-
 connection.connect();
 
-connection.query("USE 157a_team9", function (err, rows, fields) {
-    if (err) throw err;
-});
-
+//Sets up templeting the templating engine ejs, for html templates with variable inputs
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
+//Exposes a new route. req is the request object, res is the response object
 app.get("/", (req, res) => {
     connection.query(
         "SELECT * FROM 157a_team9.students;",
         function (err, rows, fields) {
             if (err) throw err;
             console.log(rows);
-            res.render("index", {
+            //Sends the html template from the views directory to the client
+            return res.render("index", {
                 data: rows,
             });
         }
     );
 });
 
+//Starts up the express web server at port 3000
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
